e98b2573421be996498166957a5b12ddc22a8ebc
Sanguis <samus941@gmail.com>
2022-04-13 21:45:58 +0300

    Adding the first project. Hello, Git!

---
+++ js/scroll.js
@@ -0,0 +1,20 @@
+const navLinks = document.querySelectorAll('a[data-goto]');
+if (navLinks.length > 0) {
+  navLinks.forEach(navLink => {
+    navLink.addEventListener("click", onNavLinkClick);
+  });
+
+  function onNavLinkClick(e) {
+    const navLink = e.target;
+    if (navLink.dataset.goto && document.querySelector(navLink.dataset.goto)) {
+      const gotoBlock = document.querySelector(navLink.dataset.goto);
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + scrollY;
+      
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth"
+      });
+      e.preventDefault();
+    }
+  }
+}
